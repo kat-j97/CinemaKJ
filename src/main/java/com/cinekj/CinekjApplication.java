3e1bc00cdfd630d5d68c60bdc5c47a67a0075f0e
jafet <jafet@LAPTOP-59QEOMGS>
2023-07-04 23:06:44 -0600

    Inicio Proyecto CinemaFinal

---
+++ src/main/java/com/cinekj/CinekjApplication.java
@@ -0,0 +1,13 @@
+package com.cinekj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinekjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinekjApplication.class, args);
+	}
+
+}
