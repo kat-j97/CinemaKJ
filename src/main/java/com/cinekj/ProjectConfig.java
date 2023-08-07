2f0cdd9f661c2bfa5b7bd9faabf3e095089ee80d
jraul1607 <134353474+jraul1607@users.noreply.github.com>
2023-08-06 22:17:17 -0600

    Login

--- src/main/java/com/cinekj/ProjectConfig.java
+++ src/main/java/com/cinekj/ProjectConfig.java
@@ -6,7 +6,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -48,4 +55,89 @@ public class ProjectConfig implements WebMvcConfigurer {
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
+    
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/registro/nuevo").setViewName("/registro/nuevo");
+    }
+
+  
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((request) -> request
+                .requestMatchers("/", "/index", "/static/static.images/**",
+                        "/registro/**", "/js/**", "/webjars/**","/css/**")
+                .permitAll()
+                .requestMatchers(
+                        "/comidas", "/contacto",
+                        "/iniciosesion", "/nosotros",
+                        "/trabajaNosotros", "/registro/usuario",
+                        "/registro/guardar"
+                ).hasRole("ADMIN")
+                .requestMatchers(
+                        "/empleado/admin",
+                        "/empleado/nuevo",
+                        "/empleado/eliminar**",
+                        "/empleado/guardar",
+                        "/empleado/modificar**",
+                        "/entrada/adminEntrada",
+    
+                        "/entrada/nuevo",
+                        "/entrada/eliminar**",
+                        "/entrada/guardar",
+                        "/entrada/modificar**",
+                        "/reporte/empleado",
+                        "/reporte/nuevo",
+    
+                        "/reporte/guardar",
+                        "/reporte/eliminar**",
+                        "/reporte/modificar**",
+                        "/entrada/modificar**"
+                       
+    
+                ).hasAnyRole("ADMIN", "EMPLEADO")
+                .requestMatchers("/entrada/adminEntrada",
+                                 "/entrada/nuevo",
+                                  "/entrada/eliminar**",
+                                    "/entrada/guardar",
+                                     "/entrada/modificar**",
+                                    "/reporte/empleado", "/reporte/nuevo",
+                                       "/reporte/guardar", "/reporte/eliminar**",
+                                        "/reporte/modificar**")
+                .hasRole("USER")
+                )
+                .formLogin((form) -> form
+                .loginPage("/login").permitAll())
+                .logout((logout) -> logout.permitAll());
+        return http.build();
+    }
+
+    /* El siguiente método se utiliza para completar la clase no es 
+    realmente funcional, la próxima semana se reemplaza con usuarios de BD */
+    @Bean
+    public UserDetailsService users() {
+        UserDetails admin = User.builder()
+                .username("juan")
+                .password("{noop}123")
+                .roles("USER", "EMPLEADO", "ADMIN")
+                .build();
+        UserDetails sales = User.builder()
+                .username("rebeca")
+                .password("{noop}456")
+                .roles("USER", "EMPLEADO")
+                .build();
+        UserDetails user = User.builder()
+                .username("pedro")
+                .password("{noop}789")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user, sales, admin);
+    }
+    
 }
